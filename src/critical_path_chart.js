55b3ef7518abb36f3842776b899d2796881564e5
Eli White <github@eli-white.com>
2016-12-10 13:29:36 -0800

    Adding a critical path chart

---
+++ src/critical_path_chart.js
@@ -0,0 +1,75 @@
+var d3 = require('d3');
+
+const CriticalPathChart = {
+  run() {
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width-100], 0);
+
+    var y = d3.scale.linear()
+        .rangeRound([height, 0]);
+
+    var color = d3.scale.category20();
+
+    var svg = d3.select("#critical-path-chart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("data.csv", function(error, data) {
+      if (error) throw error;
+
+      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "State"; }));
+
+      data.forEach(function(d) {
+        var y0 = 0;
+        d.ages = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
+        d.total = d.ages[d.ages.length - 1].y1;
+      });
+
+      x.domain(data.map(function(d) { return d.State; }));
+      y.domain([0, d3.max(data, function(d) { return d.total; })]);
+
+      var state = svg.selectAll(".state")
+          .data(data)
+        .enter().append("g")
+          .attr("class", "g")
+          .attr("transform", function(d) { return "translate(" + x(d.State) + ",0)"; });
+
+      state.selectAll("rect")
+          .data(function(d) { return d.ages; })
+        .enter().append("rect")
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.y1); })
+          .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+          .style("fill", function(d) { return color(d.name); })
+          .style("opacity", .8);
+
+      var legend = svg.selectAll(".legend")
+          .data(color.domain().slice().reverse())
+        .enter().append("g")
+          .attr("class", "legend")
+          .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+      legend.append("rect")
+          .attr("x", width - 18)
+          .attr("width", 18)
+          .attr("height", 18)
+          .style("fill", color);
+
+      legend.append("text")
+          .attr("x", width - 24)
+          .attr("y", 9)
+          .attr("dy", ".35em")
+          .style("text-anchor", "end")
+          .text(function(d) { return d; });
+
+    });
+  }
+};
+
+module.exports = CriticalPathChart;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const onDomReady = require('./on_dom_ready');
+const criticalPathChart = require('./critical_path_chart');
 
 onDomReady.queue(() => {
   document.getElementById('process-text-input').addEventListener('click', () => {
     console.log('got', document.getElementById('text-input').value);
   });
+
+  criticalPathChart.run();
 });
