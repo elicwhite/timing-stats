ff09d830ee8ff5dfd6c7d8179ee5c2716495a664
Eli White <github@eli-white.com>
2016-12-15 00:20:54 -0800

    Updating deploy script

---
+++ script/deploy.js
@@ -0,0 +1,12 @@
+var ghpages = require('gh-pages');
+var path = require('path');
+
+ghpages.publish(path.join(__dirname, '..', 'dist'), {
+  repo: 'https://' + process.env.GH_TOKEN + '@github.com/thesavior/timing-stats.git',
+  silent: true
+}, (err) => {
+  if (err) {
+    console.error('Error deploying to Github pages');
+    throw err;
+  }
+});
