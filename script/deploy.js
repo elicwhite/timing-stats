b280e4d86f0be06c0ddb12a8e1b9dc6c7e864aaa
Eli White <github@eli-white.com>
2016-12-15 01:20:36 -0800

    Adding a user to the deploy script

--- script/deploy.js
+++ script/deploy.js
@@ -4,7 +4,11 @@ var ghpages = require('gh-pages');
 var path = require('path');
 
 ghpages.publish(path.join(__dirname, '..', 'dist'), {
-  repo: 'https://' + process.env.GH_TOKEN + '@github.com/thesavior/timing-stats.git'
+  repo: 'https://' + process.env.GH_TOKEN + '@github.com/thesavior/timing-stats.git',
+  user: {
+    name: 'Eli White',
+    email: 'github@eli-white.com'
+  }
 }, (err) => {
   if (err) {
     console.error('Error deploying to Github pages');
