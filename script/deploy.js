072a272eaa7bc52833d5c95a0a98ccaf06c01176
Eli White <github@eli-white.com>
2016-12-15 01:00:45 -0800

    Not running in silent mode

--- script/deploy.js
+++ script/deploy.js
@@ -4,8 +4,7 @@ var ghpages = require('gh-pages');
 var path = require('path');
 
 ghpages.publish(path.join(__dirname, '..', 'dist'), {
-  repo: 'https://' + process.env.GH_TOKEN + '@github.com/thesavior/timing-stats.git',
-  silent: true
+  repo: 'https://' + process.env.GH_TOKEN + '@github.com/thesavior/timing-stats.git'
 }, (err) => {
   if (err) {
     console.error('Error deploying to Github pages');
