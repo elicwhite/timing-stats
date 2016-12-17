62f771e2dbe5ee37fe21f6d94b8cb8323f2fd40c
Eli White <github@eli-white.com>
2016-12-17 00:46:58 -0800

    Adding back missing script

---
+++ script/add_timing_data.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var ghpages = require('gh-pages');
+var path = require('path');
+var fs = require('fs');
+
+const rootPath = path.join(__dirname, '..');
+
+fs.appendFile(
+  path.join(path.join(rootPath, 'test_deploy_data.txt')),
+  process.env.TRAVIS_JOB_NUMBER,
+  function (err) {
+    if (err) {
+      console.error('Error writing to test_deploy_data.txt');
+      console.error(err.message);
+      console.error(err.stack);
+      process.exit(1);
+    }
+  }
+);
+
+ghpages.publish(rootPath, {
+  src: 'test_deploy_data.txt',
+  add: true,
+  branch: 'master',
+  message: 'Updating sample data [skip ci]',
+  repo: 'https://' + process.env.GH_TOKEN + '@github.com/thesavior/timing-stats.git',
+  user: {
+    name: 'Eli White',
+    email: 'github@eli-white.com'
+  }
+}, (err) => {
+  if (err) {
+    console.error('Error pushing sample data update');
+    console.error(err.message);
+    console.error(err.stack);
+    process.exit(1);
+  }
+});
